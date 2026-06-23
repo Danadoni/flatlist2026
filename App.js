8037d9cda4c6f076cfa76d8896de41b5e30c4240
danadoni.santos <danadoni.santos@NCEI-9113>
2026-06-23 13:26:35 -0300

    flatlist

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Feather from '@expo/vector-icons/Feather';
 import Banner from './src/banner';
 import Header from './src/header';
 import Search from './src/search';
+import CardMovies from './src/CardMovies';
 export default function App() {
   return (
     <View style={styles.container}>
@@ -11,7 +12,29 @@ export default function App() {
      <Search></Search>
      <Banner></Banner>
 
+    <View style = {{width:'90%'}}>
+    <FlatList
+    showsVerticalScrollIndicator= {false}
+    horizontal = {true}
+    data={Filmes}
+    keyExtractor={(item)=> item.id}
+    renderItem={({item})=> (
+
+      <CardMovies
+                titulo={item.nome}
+                imagem={item.imagem}
+                nota={item.nota}
+              />
+
+    )}
     
+    
+    
+    
+    />
+
+
+     </View>
       
 
     </View>
--- data/filmes.js
+++ data/filmes.js
@@ -1,63 +1,58 @@
-const DATA =
-  [
-    {
-        id:'01',
-        nome:'Adão Negro',
-        nota:'4.5',
-        imagem:'adao.jpg',
-
-    },
-    {
-        id:'02',
-        nome:'Avatar',
-        nota:'7.3',
-        imagem:'avatar.jpg'
-    },
-    {
-        id:'03',
-        nome:'Desaparecida',
-        nota:'8.5',
-        imagem:'desaparecida.jpg'
-    },
-    {
-        id:'04',
-        nome:'Gato de Botas II',
-        nota:'9.8',
-        imagem:'gato.jpg'
-    },
-    {
-        id:'05',
-        nome:'Homem Formiga III : QuantuMania',
-        nota:'5.8',
-        imagem:'homemformiga.jpg'
-    },
-    {
-        id:'06',
-        nome:'Os incríveis',
-        nota:'10',
-        imagem:'incriveis.jpg',
-
-    },
-    {
-        id:'07',
-        nome:'Mario',
-        nota:'8.0',
-        imagem:'mario.jpg'
-    },
-    {
-        id:'08',
-        nome:'M3GAN',
-        nota:'8.5',
-        imagem:'megan.jpg'
-    },
-    {
-        id:'09',
-        nome:'Panico VI',
-        nota:'6.5',
-        imagem:'panico.jpg'
-    }
-  
-
-]
+const DATA = [
+  {
+    id: '01',
+    nome: 'Adão Negro',
+    nota: '4.5',
+    imagem: 'https://image.tmdb.org/t/p/w500/A7vPihEtH4Q1eEDzY4Eud5AALx1.jpg',
+  },
+  {
+    id: '02',
+    nome: 'Avatar',
+    nota: '7.3',
+    imagem: 'https://image.tmdb.org/t/p/w500/jRXYjXNqO6EV6macjRqb4ZONB62.jpg',
+  },
+  {
+    id: '03',
+    nome: 'Desaparecida',
+    nota: '8.5',
+    imagem: 'https://image.tmdb.org/t/p/w500/bFjZELN515V2yW6hWv6LzB1Sht.jpg',
+  },
+  {
+    id: '04',
+    nome: 'Gato de Botas II',
+    nota: '9.8',
+    imagem: 'https://image.tmdb.org/t/p/w500/i0t5G1gZz0r3aB0uH5eX21L1M0F.jpg',
+  },
+  {
+    id: '05',
+    nome: 'Homem Formiga III : QuantuMania',
+    nota: '5.8',
+    imagem: 'https://image.tmdb.org/t/p/w500/qnqGbB22YJ7dSsjNNCIiqcbYx7c.jpg',
+  },
+  {
+    id: '06',
+    nome: 'Os incríveis',
+    nota: '10',
+    imagem: 'https://image.tmdb.org/t/p/w500/2LqaLgk4Z226KkgPJuiOQ58wvrm.jpg',
+  },
+  {
+    id: '07',
+    nome: 'Mario',
+    nota: '8.0',
+    imagem: 'https://image.tmdb.org/t/p/w500/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg',
+  },
+  {
+    id: '08',
+    nome: 'M3GAN',
+    nota: '8.5',
+    imagem: 'https://image.tmdb.org/t/p/w500/vQ5AZD8xExoI3qT2sO0Zt0K2q9A.jpg',
+  },
+  {
+    id: '09',
+    nome: 'Panico VI',
+    nota: '6.5',
+    imagem: 'https://image.tmdb.org/t/p/w500/rZpX2L4eB6L0QcI5LwWdIARlM1h.jpg',
+  }
+];
 
 export default DATA;
